2474d4858d1bbe03a03e3f93a9d9ae592338eb8c
collegelespins <73611366+collegelespins@users.noreply.github.com>
2021-04-11 22:07:18 +0200

    Update ouiqui.js

--- ouiqui.js
+++ ouiqui.js
@@ -859,8 +859,8 @@ class Page extends Div {
         new Icon(this, "Copy", dx + (ic * 4), dy, "Copier", (i) => { AddTag(this, "copy"); paste(0); });
         new Icon(this, "Paste", dx + (ic * 5), dy, "Coller", (i) => { AddTag(this, "paste"); paste(1); });
         // new Icon(this, "Delete", dx + (ic * 5), dy, "Effacer", () => { AddTag(this, "delete") });
-        new Icon(this, "Editundo", dx + (ic * 6), dy, "Annuler", () => { AddTag(this, "undo"); });
-        new Icon(this, "Editredo", dx + (ic * 7), dy, "Rétablir", () => { AddTag(this, "redo"); });
+        new Icon(this, "EditUndo", dx + (ic * 6), dy, "Annuler", () => { AddTag(this, "undo"); });
+        new Icon(this, "EditRedo", dx + (ic * 7), dy, "Rétablir", () => { AddTag(this, "redo"); });
         new Icon(this, "Align_left", dx + (ic * 8), dy, "Aligner à gauche", () => { AddTag(this, "justifyLeft"); });
         new Icon(this, "Align_center", dx + (ic * 9), dy, "Centrer", () => { AddTag(this, "justifyCenter"); });
         new Icon(this, "Align_right", dx + (ic * 10), dy, "Aligner à droite", () => { AddTag(this, "justifyRight"); });
@@ -876,7 +876,7 @@ class Page extends Div {
         new Icon(this, "Superscript", dx + (ic * 3), dy2, "Exposant", () => { AddTag(this, "superscript"); });
         new Icon(this, "Subscript", dx + (ic * 4), dy2, "Indice", () => { AddTag(this, "subscript"); });
         new Icon(this, "Link", dx + (ic * 5), dy2, "Insérer un lien hypertexte", () => AddParTag(this, "createLink", "Insérer un lien hypertexte", "Url du lien"));
-        new Icon(this, "Insertpicture", dx + (ic * 6), dy2, "Insérer une image", () => AddParTag(this, "insertImage", "Insérer une image", "Url de l'image"));
+        new Icon(this, "InsertPicture", dx + (ic * 6), dy2, "Insérer une image", () => AddParTag(this, "insertImage", "Insérer une image", "Url de l'image"));
         new Icon(this, "Addtable", dx + (ic * 7), dy2, "Insérer un tableau", () => AddTable(this, this.width));
         new Icon(this, "Play", dx + (ic * 8), dy2, "Insérer une vidéo (YouTube)", () => AddYoutube(this, this.width));
         new Icon(this, "Flv", dx + (ic * 9), dy2, "Insérer une animation swf", () => AddSwf(this));
